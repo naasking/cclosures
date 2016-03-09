7b4740769e51742c60c29a979806fddb516ede54
Sandro Magi <naasking@gmail.com>
2016-03-08 23:50:34 -0500

    added simple no-arg lifting example

--- closure/main.c
+++ closure/main.c
@@ -11,9 +11,13 @@ val_t foo(val_t arg0) {
 
 int main(int argc, char** argv) {
 	val_t arg = { .si = 99789 };
-	clo_t c = clo_lift((fn_t)foo, 1, 1, arg);
+	clo_t c = clo_lift((fn_t){ .fn1 = foo }, 1, 1, arg);
 	printf("c->env = %d\n", c->env[0].si);
 	val_t x = clo_apply0(c);
+	printf("x = %u\n", x.si);
+
+	clo_t c2 = clo_lift((fn_t) { .fn1 = foo }, 1, 0);
+	x = clo_apply1(c2, arg);
 	printf("x = %u\n", x.si);
 	return 0;
 }
